dc575b1ea583a55e5c7205faa5dfc6dde42a2cd5
Nahuel Maika <43748108+NahuelMaika@users.noreply.github.com>
2026-06-08 20:45:26 -0300

    refactor(db): switch to PrismaNeon adapter for edge compatibility

--- lib/queries/product.queries.ts
+++ lib/queries/product.queries.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/db/prisma";
+import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 import { convertToPlainObject } from "../utils";
 
