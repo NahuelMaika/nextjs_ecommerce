4ae8359a3cfd3458dd83ca4320cd7bc8b3e56652
Nahuel Maika <nahuelmaika@gmail.com>
2026-06-13 19:13:08 -0300

    feat: implement first look of ProductDetailsPage

--- lib/queries/product.queries.ts
+++ lib/queries/product.queries.ts
@@ -11,3 +11,11 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(products);
 }
+
+//Get single product by slug
+export async function getProductBySlug(slug: string) {
+  const product = await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+  return product;
+}
